4e8de028cd7b1f8e550b2d0b40c32c65ae4f0009
Nick Char <nick.char.nick@gmail.com>
2017-06-20 09:43:09 -0400

    Add snack actions and instructions

---
+++ food_stand/frontend/actions/snackActions.js
@@ -0,0 +1,11 @@
+/*
+In this file we will define our Snack Actions!
+
+Look at fruitActions as a reference if you get stuck.
+// NOTE: Remember to export!
+*/
+
+// 1. Define action constant
+
+
+// 2. Define addSnack action and clearSnacks
