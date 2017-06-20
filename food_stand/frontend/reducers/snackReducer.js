a7e41893cd54ddfa1deec0c19d258a1f41af7a4d
Nick Char <nick.char.nick@gmail.com>
2017-06-20 09:49:57 -0400

    Add snack Reducer file and instructions

---
+++ food_stand/frontend/reducers/snackReducer.js
@@ -0,0 +1,24 @@
+/*
+In this file we will define our snack Reducer.
+
+Be sure to import your snack action constants and the associated actions.
+
+Also remember that you do not want to mutate state!
+Always return a brand new object as state instead.
+
+Export your reducer at the end!
+
+As before, refer to the fruitReducer if you get stuck.
+*/
+
+// 1. Import
+
+// 2. Define _defaultState
+
+// 3. Define your snackReducer, which takes your old / previousState
+//    and a the action as arguments and returns the newState.
+//
+//
+//    3.1 Use Object.freeze to prevent mutation of oldState.
+//    3.2 Use a switch statement to handle each possible action.
+//    3.3 Remember to return oldState by default.
