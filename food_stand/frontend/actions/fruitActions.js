8258e06ef2c2c137c651c7d68a1ba07035417451
Nick Char <nick.char.nick@gmail.com>
2017-06-20 09:43:30 -0400

    Add instructions, remove hotdog

--- food_stand/frontend/actions/fruitActions.js
+++ food_stand/frontend/actions/fruitActions.js
@@ -1,18 +1,18 @@
 export const ADD_FRUIT = "ADD_FRUIT";
 export const CLEAR_FOOD = "CLEAR_FOOD";
 
-// export const addApple = () => {
-//
-//   return {
-//     type: ADD_FRUIT,
-//     fruit: 'Apple'
-//   };
-// };
-//
-// export const addOrange = () => ({ // impicit return version, no debugging options
-//   type: ADD_FRUIT,
-//   fruit: 'Orange'
-// });
+export const addApple = () => {
+
+  return {
+    type: ADD_FRUIT,
+    fruit: 'Apple'
+  };
+};
+
+export const addOrange = () => ({ // impicit return version, no debugging options
+  type: ADD_FRUIT,
+  fruit: 'Orange'
+});
 
 export const clearFood = () => {
   return {
@@ -20,12 +20,12 @@ export const clearFood = () => {
   };
 };
 
-export const addFruit = (fruit) => {
-  return {
-    type: ADD_FRUIT,
-    fruit
-  };
-};
-
-export const addApple = () => addFruit('Apple');
-export const addOrange = () => addFruit('Orange');
+// export const addFruit = (fruit) => {
+//   return {
+//     type: ADD_FRUIT,
+//     fruit
+//   };
+// };
+//
+// export const addApple = () => addFruit('Apple');
+// export const addOrange = () => addFruit('Orange');
--- food_stand/frontend/actions/hotDogActions.js
+++
@@ -1,7 +0,0 @@
-export const ADD_HOTDOG = "ADD_HOTDOG";
-
-export const addHotDog = () => {
-  return {
-    type: ADD_HOTDOG
-  };
-};
